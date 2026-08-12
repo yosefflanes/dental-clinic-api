8e15eb9487d1ac02cc8eddca3be74e0db2609dac
Yosef Fernando Lanes <yosefflanes@gmail.com>
2026-08-12 09:54:03 +0700

    Add gender and date_of_birth fields to users table migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->enum('gender', ['pria', 'wanita'])->nullable();
+            $table->date('date_of_birth')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('phone')->nullable();

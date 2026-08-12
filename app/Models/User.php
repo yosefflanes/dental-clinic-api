012c8ec7c13239114946469ec9b80aa025b3f416
Yosef Fernando Lanes <yosefflanes@gmail.com>
2026-08-12 09:55:26 +0700

    Add gender and date_of_birth to fillable attributes and update casts in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,8 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'gender',
+        'date_of_birth',
         'password',
         'phone',
         'role'
@@ -38,7 +40,8 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
+            'password'          => 'hashed',
+            'date_of_birth'     => 'date',
         ];
     }
 

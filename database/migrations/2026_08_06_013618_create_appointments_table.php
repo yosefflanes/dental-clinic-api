40d7ff474168a0d4df3f2f7d5d062b9348cb550a
Yosef Fernando Lanes <yosefflanes@gmail.com>
2026-08-19 11:15:41 +0700

    feat: create appointments migration table with user, service, and schedule relations

--- database/migrations/2026_08_06_013618_create_appointments_table.php
+++ database/migrations/2026_08_06_013618_create_appointments_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -15,7 +14,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->foreignId('service_id')->constrained('services')->cascadeOnDelete();
-            $table->foreignId('doctor_schedule_id')->unique()->constrained('doctor_schedules')->cascadeOnDelete();
+            $table->foreignId('doctor_schedule_id')->constrained('doctor_schedules')->cascadeOnDelete();
             $table->text('complaint')->nullable();
             $table->enum('status', ['pending', 'selesai', 'batal'])->default('pending')->index();
             $table->timestamps();

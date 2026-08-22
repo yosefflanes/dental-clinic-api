8e1f6cb0970fedf199d6b7483fe0a29fd7b6cd8c
Yosef Fernando Lanes <yosefflanes@gmail.com>
2026-08-22 12:33:21 +0700

    feat: add login route with unauthenticated error response

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// routes/web.php
+Route::get('/login', function () {
+    return response()->json([
+        'status' => 'error',
+        'message' => 'Unauthenticated.'
+    ], 401);
+})->name('login');
